966ed6900c696a902fcc4a6856f626f315d8996d
Mykola Losyk <mykola.losyk@gmail.com>
2026-01-18 17:04:51 +0200

    was added new api also added new f

---
+++ tmp copy.js
@@ -0,0 +1,292 @@
+[
+    {
+        "lon": 14.464647263243574,
+        "lat": 50.04503751828389,
+        "label": "📍 Na Záhonech 916/26"
+    },
+    {
+        "lon": 14.38935,
+        "lat": 49.90808,
+        "label": "Hlavní 191, Měchenice, 252 06, Praha-západ",
+        "duplicateCount": 4
+    },
+    {
+        "lon": 14.38493,
+        "lat": 49.90478,
+        "label": "Na Vyhlídce 346, Měchenice, 252 06, Praha-západ"
+    },
+    {
+        "lon": 14.39183,
+        "lat": 49.90271,
+        "label": "Konečná 315, Měchenice, 252 06, Praha-západ"
+    },
+    {
+        "lon": 14.39257,
+        "lat": 49.90795,
+        "label": "Přístavní 1, Měchenice, 252 06, Praha-západ"
+    },
+    {
+        "lon": 14.38755,
+        "lat": 49.90505,
+        "label": "Na Vyhlídce 183, Měchenice, 252 06, Praha-západ"
+    },
+    {
+        "lon": 14.39732,
+        "lat": 49.89495,
+        "label": "V kruhovce 311, Davle, 252 06, Praha-západ"
+    },
+    {
+        "lon": 14.4001,
+        "lat": 49.89194,
+        "label": "Vltavská 117, Davle, 252 06, Praha-západ"
+    },
+    {
+        "lon": 14.39939,
+        "lat": 49.88056,
+        "label": "Pikovická ev. č. 86, Davle, 252 06, Praha-západ"
+    },
+    {
+        "lon": 14.39526,
+        "lat": 49.88891,
+        "label": "K Pivovaru 302, Davle, 252 06, Praha-západ"
+    },
+    {
+        "lon": 14.39444,
+        "lat": 49.89121,
+        "label": "Na Palouku 348, Davle, 252 06, Praha-západ"
+    },
+    {
+        "lon": 14.39113,
+        "lat": 49.89277,
+        "label": "U Vodojemu 115, Davle, 252 06, Praha-západ"
+    },
+    {
+        "lon": 14.37491,
+        "lat": 49.888,
+        "label": "Družstevní 4, Davle, 252 06, Praha-západ"
+    },
+    {
+        "lon": 14.37726,
+        "lat": 49.8878,
+        "label": "K Borovíčku 16, Davle, 252 06, Praha-západ"
+    },
+    {
+        "lon": 14.37684,
+        "lat": 49.89186,
+        "label": "U Jeřábu 139, Davle, 252 06, Praha-západ"
+    },
+    {
+        "lon": 14.39842,
+        "lat": 49.88574,
+        "label": "U Lípy 40, Davle, 252 06, Praha-západ"
+    },
+    {
+        "lon": 14.37426,
+        "lat": 49.87124,
+        "label": "Hvozdnice 6, Hvozdnice, 252 05, Praha-západ"
+    },
+    {
+        "lon": 14.363,
+        "lat": 49.87464,
+        "label": "Hvozdnice 237, Hvozdnice, 252 05, Praha-západ",
+        "duplicateCount": 2
+    },
+    {
+        "lon": 14.36352,
+        "lat": 49.87056,
+        "label": "Hvozdnice 225, Hvozdnice, 252 05, Praha-západ"
+    },
+    {
+        "lon": 14.3672,
+        "lat": 49.87336,
+        "label": "Hvozdnice 213, Hvozdnice, 252 05, Praha-západ"
+    },
+    {
+        "lon": 14.3716,
+        "lat": 49.87241,
+        "label": "Hvozdnice 59, Hvozdnice, 252 05, Praha-západ"
+    },
+    {
+        "lon": 14.36641,
+        "lat": 49.87344,
+        "label": "Hvozdnice 90, Hvozdnice, 252 05, Praha-západ"
+    },
+    {
+        "lon": 14.34706,
+        "lat": 49.85839,
+        "label": "Bojanovice 109, Bojanovice, 252 06, Praha-západ"
+    },
+    {
+        "lon": 14.35283,
+        "lat": 49.85586,
+        "label": "Bojanovice 89, Bojanovice, 252 06, Praha-západ"
+    },
+    {
+        "lon": 14.3526,
+        "lat": 49.85413,
+        "label": "Bojanovice 10, Bojanovice, 252 06, Praha-západ"
+    },
+    {
+        "lon": 14.33895,
+        "lat": 49.84503,
+        "label": "Bratřínov ev. č. 147, Bratřínov, 252 05, Praha-západ"
+    },
+    {
+        "lon": 14.35734,
+        "lat": 49.85563,
+        "label": "Bojanovice 49, Bojanovice, 252 06, Praha-západ"
+    },
+    {
+        "lon": 14.37935,
+        "lat": 49.85207,
+        "label": "Masečín 31, Štěchovice, 252 07, Praha-západ"
+    },
+    {
+        "lon": 14.3831,
+        "lat": 49.85347,
+        "label": "Masečín 201, Štěchovice, 252 07, Praha-západ"
+    },
+    {
+        "lon": 14.39349,
+        "lat": 49.85744,
+        "label": "Masečín ev. č. 585, Štěchovice, 252 07, Praha-západ"
+    },
+    {
+        "lon": 14.40557,
+        "lat": 49.85117,
+        "label": "Hlavní 118, Štěchovice, 252 07, Praha-západ",
+        "duplicateCount": 2
+    },
+    {
+        "lon": 14.40473,
+        "lat": 49.85156,
+        "label": "Hlavní 14, Štěchovice, 252 07, Praha-západ"
+    },
+    {
+        "lon": 14.39062,
+        "lat": 49.85092,
+        "label": "Štěchovice ev. č. 4, Štěchovice, 252 07, Praha-západ"
+    },
+    {
+        "lon": 14.4108,
+        "lat": 49.84709,
+        "label": "Pod Havranem 193, Štěchovice, 252 07, Praha-západ"
+    },
+    {
+        "lon": 14.41183,
+        "lat": 49.84719,
+        "label": "Pod Havranem 183, Štěchovice, 252 07, Praha-západ"
+    },
+    {
+        "lon": 14.41316,
+        "lat": 49.84971,
+        "label": "Elektrárenská 248, Štěchovice, 252 07, Praha-západ"
+    },
+    {
+        "lon": 14.41217,
+        "lat": 49.85185,
+        "label": "Vltavská 368, Štěchovice, 252 07, Praha-západ"
+    },
+    {
+        "lon": 14.4106,
+        "lat": 49.85155,
+        "label": "Na Peškově 229, Štěchovice, 252 07, Praha-západ"
+    },
+    {
+        "lon": 14.41058,
+        "lat": 49.85013,
+        "label": "Nad Pískovnou 214, Štěchovice, 252 07, Praha-západ"
+    },
+    {
+        "lon": 14.40147,
+        "lat": 49.85843,
+        "label": "Nad Keramem 710, Hradištko, 252 09, Praha-západ"
+    },
+    {
+        "lon": 14.40184,
+        "lat": 49.85747,
+        "label": "Nad Keramem 69, Hradištko, 252 09, Praha-západ",
+        "duplicateCount": 7
+    },
+    {
+        "lon": 14.40903,
+        "lat": 49.85481,
+        "label": "Na Ovčičkách 252, Hradištko, 252 09, Praha-západ",
+        "duplicateCount": 2
+    },
+    {
+        "lon": 14.4061,
+        "lat": 49.86657,
+        "label": "Velká Podkova 133, Hradištko, 252 09, Praha-západ"
+    },
+    {
+        "lon": 14.41534,
+        "lat": 49.86957,
+        "label": "Pod Vodojemem 550, Hradištko, 252 09, Praha-západ"
+    },
+    {
+        "lon": 14.41396,
+        "lat": 49.86863,
+        "label": "Spojovací ev. č. 1662, Hradištko, 252 09, Praha-západ"
+    },
+    {
+        "lon": 14.4167,
+        "lat": 49.86961,
+        "label": "V Zákoutí 551, Hradištko, 252 09, Praha-západ"
+    },
+    {
+        "lon": 14.41502,
+        "lat": 49.8697,
+        "label": "Pod Vodojemem 228, Hradištko, 252 09, Praha-západ"
+    },
+    {
+        "lon": 14.39976,
+        "lat": 49.86932,
+        "label": "K Dubí 70, Hradištko, 252 09, Praha-západ"
+    },
+    {
+        "lon": 14.45687,
+        "lat": 49.85592,
+        "label": "Třebsín 8, Krňany, 257 44, Benešov"
+    },
+    {
+        "lon": 14.51334,
+        "lat": 49.79643,
+        "label": "Všetice 10, Netvořice, 257 44, Benešov"
+    },
+    {
+        "lon": 14.43585,
+        "lat": 49.81408,
+        "label": "Rabyně 11, Rabyně, 252 08, Benešov"
+    },
+    {
+        "lon": 14.3962,
+        "lat": 49.81391,
+        "label": "Slapy 280, Slapy, 252 08, Praha-západ"
+    },
+    {
+        "lon": 14.39419,
+        "lat": 49.817,
+        "label": "Slapy 15, Slapy, 252 08, Praha-západ"
+    },
+    {
+        "lon": 14.39529,
+        "lat": 49.81628,
+        "label": "Slapy 37, Slapy, 252 08, Praha-západ"
+    },
+    {
+        "lon": 14.40547,
+        "lat": 49.81204,
+        "label": "Slapy 398, Slapy, 252 08, Praha-západ"
+    },
+    {
+        "lon": 14.38725,
+        "lat": 49.79997,
+        "label": "Buš 175, Buš, 252 08, Praha-západ"
+    },
+    {
+        "lon": 14.37863,
+        "lat": 49.78926,
+        "label": "Buš 44, Buš, 252 08, Praha-západ"
+    }
+]
